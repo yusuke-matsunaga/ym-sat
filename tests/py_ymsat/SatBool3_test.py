87a94547d64f9bf65a45d3512fdfe6b81b8266eb
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2022-12-29 01:00:04 +0900

    tests/py_ymsat/SatBool3_test.py: テストを追加

--- tests/py_ymsat/SatBool3_test.py
+++ tests/py_ymsat/SatBool3_test.py
@@ -12,16 +12,69 @@ import ymsat
 v1 = ymsat.SatBool3('true')
 assert str(v1) == "true"
 assert v1 == ymsat.SatBool3.true
+assert v1 != ymsat.SatBool3.false
+assert v1 != ymsat.SatBool3.x
 
 v2 = ymsat.SatBool3('False')
 assert str(v2) == "false"
+assert v2 != ymsat.SatBool3.true
 assert v2 == ymsat.SatBool3.false
+assert v2 != ymsat.SatBool3.x
 
 v3 = ymsat.SatBool3('x')
 assert str(v3) == "x"
+assert v3 != ymsat.SatBool3.true
+assert v3 != ymsat.SatBool3.false
 assert v3 == ymsat.SatBool3.x
 
 assert ~v1 == v2
 assert ~v2 == v1
 assert ~v3 == v3
 
+assert v1 ^ v1 == ymsat.SatBool3.false
+assert v1 ^ v2 == ymsat.SatBool3.true
+assert v1 ^ v3 == ymsat.SatBool3.x
+
+assert v2 ^ v1 == ymsat.SatBool3.true
+assert v2 ^ v2 == ymsat.SatBool3.false
+assert v2 ^ v3 == ymsat.SatBool3.x
+
+assert v3 ^ v1 == ymsat.SatBool3.x
+assert v3 ^ v2 == ymsat.SatBool3.x
+assert v3 ^ v3 == ymsat.SatBool3.x
+
+v = v1
+v ^= v1
+v == ymsat.SatBool3.false
+
+v = v1
+v ^= v2
+v == ymsat.SatBool3.true
+
+v = v1
+v ^= v3
+v == ymsat.SatBool3.x
+
+v = v2
+v ^= v1
+v == ymsat.SatBool3.true
+
+v = v2
+v ^= v2
+v == ymsat.SatBool3.false
+
+v = v2
+v ^= v3
+v == ymsat.SatBool3.x
+
+v = v3
+v ^= v1
+v == ymsat.SatBool3.x
+
+v = v3
+v ^= v2
+v == ymsat.SatBool3.x
+
+v = v3
+v ^= v3
+v == ymsat.SatBool3.x
