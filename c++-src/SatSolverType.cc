b01ade8603b88fa0fa892ca60de3269c1594cf65
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2023-05-24 13:06:57 +0900

    SatSolverType 変更中

---
+++ c++-src/SatSolverType.cc
@@ -0,0 +1,92 @@
+﻿
+/// @file SatSolverType.cc
+/// @brief SatSolverType の実装ファイル
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2023 Yusuke Matsunaga
+/// All rights reserved.
+
+#include "ym/SatSolverType.h"
+
+
+BEGIN_NAMESPACE_YM_SAT
+
+//////////////////////////////////////////////////////////////////////
+// SatSolverType
+//////////////////////////////////////////////////////////////////////
+
+// @brief コンストラクタ(タイプを表す文字列)
+SatSolverType::SatSolverType(
+  const string& type
+)
+{
+  mJsObj["type"] = Json::Value{type};
+  check_type();
+}
+
+// @brief コンストラクタ(Jsonオブジェクト)
+SatSolverType::SatSolverType(
+  const Json::Value& js_obj
+) : mJsObj{js_obj}
+{
+  check_type();
+}
+
+// @brief 実装タイプを返す．
+const string&
+SatSolverType::type() const
+{
+  if ( mJsObj.isMember("type") ) {
+    return
+}
+
+SatSolverType::SatSolverType(
+  const string& type,
+  const string& option,
+  ostream* log_out
+) : mType{type},
+    mOption{option},
+    mLogOut{log_out}
+{
+}
+
+// @brief type のチェックを行う．
+void
+SatSolverType::check_type()
+{
+  if ( mType == "minisat" ) {
+    // minisat-1.4
+    ;
+  }
+  else if ( mType == "minisat2" ) {
+    // minisat-2.2
+    ;
+  }
+  else if ( mType == "glueminisat2" ) {
+    // glueminisat-2.2.8
+    ;
+  }
+  else if ( mType == "lingeling" ) {
+    ;
+  }
+  else if ( mType == "ymsat1" ) {
+    ;
+  }
+  else if ( mType == "ymsat2" ) {
+    ;
+  }
+  else if ( mType == "ymsat1_old" ) {
+    ;
+  }
+  else if ( mType == "" ) {
+    // lingeling 今はデフォルトにしている．
+    mType = "lingeling";
+  }
+  else {
+    mType = "lingeling";
+    cerr << "SatSolver: unknown type '" << type << "', '"
+	 << mType << "' is used, instead." << endl;
+  }
+}
+
+END_NAMESPACE_YM_SAT
--- include/ym/SatSolverType.h
+++ include/ym/SatSolverType.h
@@ -9,6 +9,7 @@
 /// All rights reserved.
 
 #include "ym/sat.h"
+#include <json/json.h>
 
 
 BEGIN_NAMESPACE_YM_SAT
@@ -16,16 +17,22 @@ BEGIN_NAMESPACE_YM_SAT
 //////////////////////////////////////////////////////////////////////
 /// @class SatSolverType SatSolverType.h "ym/SatSolverType.h"
 /// @brief SATソルバの種類を表すクラス
+///
+/// 単純な場合はタイプを表す文字列のみを引数にとる．
+/// 詳細な設定を行う場合は Json オブジェクトを引数にとる．
 //////////////////////////////////////////////////////////////////////
 class SatSolverType
 {
 public:
 
-  /// @brief コンストラクタ
+  /// @brief コンストラクタ(タイプを表す文字列)
   SatSolverType(
-    const string& type = string{},   ///< [in] 実装タイプを表す文字列
-    const string& option = string{}, ///< [in] オプション文字列
-    ostream* log_out = nullptr       ///< [in] ログを記録するストリームへのポインタ
+    const string& type = string{} ///< [in] 実装タイプを表す文字列
+  );
+
+  /// @brief コンストラクタ(Jsonオブジェクト)
+  SatSolverType(
+    const Json::Value& js_obj ///< [in] オプションを表す Json オブジェクト
   );
 
   /// @brief デストラクタ
@@ -37,39 +44,28 @@ public:
   // 外部インターフェイス
   //////////////////////////////////////////////////////////////////////
 
-  /// @brief 内容をセットする．
-  void
-  set(
-    const string& type,               ///< [in] 実装タイプを表す文字列
-    const string& option = string{},  ///< [in] オプション文字列
-    ostream* log_out = nullptr	      ///< [in] ログを記録するストリームへのポインタ
-  )
-  {
-    mType = type;
-    mOption = option;
-    mLogOut = log_out;
-  }
-
   /// @brief 実装タイプを返す．
   const string&
-  type() const
+  type() const;
+
+  /// @brief オプションを表す Json オブジェクトを返す．
+  const Json::Value&
+  js_obj() const
   {
-    return mType;
+    return mJsObj;
   }
 
-  /// @brief オプションを返す．
-  const string&
-  option() const
-  {
-    return mOption;
-  }
 
-  /// @brief ログ出力のストリームを返す．
-  ostream*
-  log_out() const
-  {
-    return mLogOut;
-  }
+private:
+  //////////////////////////////////////////////////////////////////////
+  // 内部で用いられる関数
+  //////////////////////////////////////////////////////////////////////
+
+  /// @brief type のチェックを行う．
+  ///
+  /// エラー時には例外が創出される．
+  void
+  check_type();
 
 
 private:
@@ -77,14 +73,8 @@ private:
   // データメンバ
   //////////////////////////////////////////////////////////////////////
 
-  // 実装タイプ
-  string mType;
-
-  // オプション
-  string mOption;
-
-  // ログ出力先
-  ostream* mLogOut;
+  // 本体
+  Json::Value mJsObj;
 
 };
 
